f6b3ca18f86b07c586ba7b06ddecc7628757aa4a
sjsafranek <sjsafranek@gmail.com>
2023-09-28 00:47:06 +0100

    advanced json encoders and decoders

--- PersonNameRecognition/chain.py
+++ PersonNameRecognition/chain.py
@@ -2,6 +2,85 @@ import json
 import functools
 
 
+
+# https://mathspp.com/blog/custom-json-encoder-and-decoder
+
+
+
+def mean(numbers):
+	return sum(numbers) / len(numbers)	
+
+
+
+
+class ExtendedDecoder(json.JSONDecoder):
+	def __init__(self, **kwargs):
+		kwargs["object_hook"] = self.object_hook
+		super().__init__(**kwargs)
+
+	def object_hook(self, obj):
+		try:
+			name = obj["__extended_json_type__"]
+			decoder = getattr(self, f"decode_{name}")
+		except (KeyError, AttributeError):
+			return obj
+		else:
+			return decoder(obj)
+
+
+class ExtendedEncoder(json.JSONEncoder):
+	def default(self, obj):
+		name = type(obj).__name__
+		try:
+			encoder = getattr(self, f"encode_{name}")
+		except AttributeError:
+			super().default(obj)
+		else:
+			encoded = encoder(obj)
+			encoded["__extended_json_type__"] = name
+			return encoded
+
+
+class ChainEncoder(ExtendedEncoder):
+	def encode_set(self, obj):
+		return {"set": list(obj)}
+
+	def encode_complex(self, c):
+		return {"real": c.real, "imag": c.imag}
+
+	def encode_range(self, r):
+		return {"start": r.start, "stop": r.stop, "step": r.step}
+
+
+class ChainDecoder(ExtendedDecoder):
+	def decode_set(self, obj):
+		return set(obj["set"])
+
+	def decode_complex(self, obj):
+		return complex(obj["real"], obj["imag"])
+
+	def decode_range(self, obj):
+		return range(obj["start"], obj["stop"], obj["step"])
+
+
+
+
+# class SetEncoder(json.JSONEncoder):
+# 	def default(self, obj):
+# 		if isinstance(obj, set):
+# 			return list(obj)
+# 		return json.JSONEncoder.default(self, obj)
+
+
+
+# class SetDecoder(json.JSONDecoder):
+# 	def default(self, obj):
+# 		if isinstance(obj, list):
+# 			return set(obj)
+# 		return json.JSONDecoder.default(self, obj)
+
+
+
 class Chain(object):
 
 	def __init__(self, size=1):
@@ -10,10 +89,13 @@ class Chain(object):
 		self.size = size
 		self.data = {}
 	
+	def _newCollection(self):
+		return set()
+
 	@functools.cache
 	def getTokens(self, text):
 		text = text.lower()
-		text = '_' + text + '_'
+		text = '_{text}_'.format(text=text)
 		#return [text[i:i+self.size] for i in range(0, len(text), 1) if i+self.size-1 < len(text)]
 		return [text[i:i+self.size] for i in range(0, len(text), 3) if i+self.size-1 < len(text)]
 
@@ -28,9 +110,11 @@ class Chain(object):
 		previous = None
 		for current in self.getTokens(text):
 			if current not in self.data:
-				self.data[current] = []
+				#self.data[current] = []
+				self.data[current] = self._newCollection()
 			if None != previous and current not in self.data[previous]:
-				self.data[previous].append(current)
+				#self.data[previous].append(current)
+				self.data[previous].add(current)
 			previous = current
 
 	@functools.cache
@@ -46,7 +130,7 @@ class Chain(object):
 			scores.append(self._calculateScore(text))
 		if 0 == len(scores):
 			return 0
-		return sum(scores) / len(scores)
+		return mean(scores)
 
 	def _calculateScore(self, text):
 		scores = []
@@ -61,14 +145,16 @@ class Chain(object):
 						score = 1
 			previous = current
 			scores.append(score)
-		return 100 * (sum(scores) / len(scores))		
+		return 100 * mean(scores)		
 
 	def load(self, filename):
 		with open(filename, 'r', encoding="utf-8") as fh:
-			data = json.loads(fh.read())
-			self.data = data
+			raw = fh.read()
+			self.data = json.loads(raw, cls=ChainDecoder)
+		print(type(list(self.data.values())[0]))
 
 	def dump(self, filename):
+		print(type(list(self.data.values())[0]))
 		with open(filename, 'w', encoding="utf-8") as fh:
-			fh.write(json.dumps(self.data))
-
+			raw = json.dumps(self.data, cls=ChainEncoder)
+			fh.write(raw)
--- PersonNameRecognition/main.py
+++ PersonNameRecognition/main.py
@@ -15,7 +15,14 @@ else:
 			line = line.replace('\n', '')
 			chain.learn(line)
 	chain.dump('data/chain.json')
+chain.dump('data/chain.json')
 
+
+# with open('data/name_corpus.txt', 'r', encoding="utf-8") as fh:
+# 	for line in fh.readlines():
+# 		line = line.replace('\n', '')
+# 		chain.learn(line)
+# chain.dump('data/chain.json')
 
 
 score = chain.calculateScore("stefan safranek")
